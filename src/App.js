f3b58096f17515159257d4ee75841b048f815022
Krituraaj Anand <kjais2000@gmail.com>
2022-04-03 00:29:59 +0530

    Created middleware and updated routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import "./App.css";
 import { Header, Footer } from "components";
-import {AllRoutes} from "./AllRoutes";
-
+import { AllRoutes } from "./AllRoutes";
+import { useAuth } from "contexts";
+import { Toast } from "components";
+import { AuthModal } from "pages"
 function App() {
+  const { showAuthModal } = useAuth();
   return (
     <div className="App bg-primary">
-      <Header/>
-      <AllRoutes/>
-      <Footer/>
+      <Header />
+      <AllRoutes />
+      <Footer />
+      <Toast />
+      {showAuthModal ? <AuthModal /> : " "}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { makeServer } from "./server";
 import { BrowserRouter as Router } from "react-router-dom";
+import { AuthProvider } from "contexts";
 // Call make Server
 makeServer();
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </Router>
   </React.StrictMode>,
   document.getElementById("root")
