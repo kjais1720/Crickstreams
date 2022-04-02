8c5af11eea1badcb11e614824f78b727e9a0d014
Krituraaj Anand <kjais2000@gmail.com>
2022-04-03 00:30:23 +0530

    Created Loaders and Toast

---
+++ src/components/toast.jsx
@@ -0,0 +1,11 @@
+import { ToastContainer } from "react-toastify"
+import 'react-toastify/dist/ReactToastify.css';
+
+export function Toast(){
+  return(
+  <ToastContainer
+    position={"bottom-right"}
+    autoClose={4000}
+    draggable
+  />)
+}
